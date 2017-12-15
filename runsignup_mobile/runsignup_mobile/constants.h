5e44bdd56bec8c8e11e7f5d070087bba2009fb81
Jeff Kiesel <jeff.kiesel@runsignup.com>
2017-12-15 11:01:43 -0500

    added new test entry points, corrected bundle ID from "com.runsignup.runsignup-mobile-go" to "com.runsignup.runsignup-mobile"

--- runsignup_mobile/runsignup_mobile/constants.h
+++ runsignup_mobile/runsignup_mobile/constants.h
@@ -8,13 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
-// RSU ENTRY POINT
+// RSU ENTRY POINT PRODUCTION
 //#define RSU_DOMAIN  @"runsignup.com"
-//#define RSU_ENTRY_POINT_SERVER_URL  @"https://runsignup.com/Races"
+//#define RSU_ENTRY_POINT_SERVER_URL  @"https://runsignup.com/MobileRaces"
 
-//// TESTING ENTRY POINT: local
+// Testing Entry Point: jk local: localhost
+//#define RSU_DOMAIN  @"http://localhost"
+//#define RSU_ENTRY_POINT_SERVER_URL  @"http://localhost/MobileRaces"
+
+// Testing Entry Point: jk local: 10.1.10.68
 //#define RSU_DOMAIN  @"10.1.10.68"
 //#define RSU_ENTRY_POINT_SERVER_URL  @"http://10.1.10.68/MobileRaces"
+
+// Testing Entry Point: jk home local: 10.0.0.21
+//#define RSU_DOMAIN  @"10.0.0.21"
+//#define RSU_ENTRY_POINT_SERVER_URL  @"http://10.0.0.21/MobileRaces"
+
+// Testing Entry Point: local: hosts file redirects test.runsignup.com to 10.1.10.68
+//#define RSU_DOMAIN  @"test.runsignup.com"
+//#define RSU_ENTRY_POINT_SERVER_URL  @"http://test.runsignup.com/MobileRaces"
+
+// jh local
 //#define RSU_DOMAIN  @"192.168.0.9"
 //#define RSU_ENTRY_POINT_SERVER_URL  @"http://192.168.0.9:8082"
 
@@ -25,6 +39,6 @@
 #define RSU_URL_PROTOCOL_KEY   @"RSUURLProtocolHandledKey"
 
 // Custom Header Fields
-#define CUSTOM_HEADER_MOBILE_TYPE_FIELDNAME   @"rsu_mobile_type"
+#define CUSTOM_HEADER_MOBILE_TYPE_FIELDNAME   @"x-rsu-mobile-type"
 #define CUSTOM_HEADER_MOBILE_TYPE_FIELDVALUE  @"ios"
 

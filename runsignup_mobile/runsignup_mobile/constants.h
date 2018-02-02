1625fc1c011956f5c680c0970d89ad5e2e832f20
Jeff Kiesel <jeff.kiesel@runsignup.com>
2018-02-02 11:10:29 -0500

    changed url from RacesMobile to just Races. changed forward and back arrows to single arrows

--- runsignup_mobile/runsignup_mobile/constants.h
+++ runsignup_mobile/runsignup_mobile/constants.h
@@ -10,24 +10,24 @@
 
 // RSU ENTRY POINT PRODUCTION
 //#define RSU_DOMAIN  @"runsignup.com"
-//#define RSU_ENTRY_POINT_SERVER_URL  @"https://runsignup.com/MobileRaces"
+//#define RSU_ENTRY_POINT_SERVER_URL  @"https://runsignup.com/Races"
 
 // Testing Entry Point: jk local: localhost
 //#define RSU_DOMAIN  @"http://localhost"
-//#define RSU_ENTRY_POINT_SERVER_URL  @"http://localhost/MobileRaces"
+//#define RSU_ENTRY_POINT_SERVER_URL  @"http://localhost/Races"
 
 // Testing Entry Point: jk local: 10.1.10.68
 //#define RSU_DOMAIN  @"10.1.10.68"
-//#define RSU_ENTRY_POINT_SERVER_URL  @"http://10.1.10.68/MobileRaces"
-//#define RSU_ENTRY_POINT_SERVER_URL  @"http://172.20.20.20/MobileRaces"
+//#define RSU_ENTRY_POINT_SERVER_URL  @"http://10.1.10.68/Races"
+//#define RSU_ENTRY_POINT_SERVER_URL  @"http://172.20.20.20/Races"
 
 // Testing Entry Point: jk home local: 10.0.0.21
 //#define RSU_DOMAIN  @"10.0.0.21"
-//#define RSU_ENTRY_POINT_SERVER_URL  @"http://10.0.0.21/MobileRaces"
+//#define RSU_ENTRY_POINT_SERVER_URL  @"http://10.0.0.21/Races"
 
 // Testing Entry Point: local: hosts file redirects test.runsignup.com to 10.1.10.68
 //#define RSU_DOMAIN  @"test.runsignup.com"
-//#define RSU_ENTRY_POINT_SERVER_URL  @"http://test.runsignup.com/MobileRaces"
+//#define RSU_ENTRY_POINT_SERVER_URL  @"http://test.runsignup.com/Races"
 
 // jh local
 //#define RSU_DOMAIN  @"192.168.0.9"
@@ -35,7 +35,7 @@
 
 // TESTING ENTRY POINT: test4.runsignup.com
 #define RSU_DOMAIN  @"test4.runsignup.com"
-#define RSU_ENTRY_POINT_SERVER_URL  @"http://test4.runsignup.com/MobileRaces"
+#define RSU_ENTRY_POINT_SERVER_URL  @"http://test4.runsignup.com/Races"
 
 #define RSU_URL_PROTOCOL_KEY   @"RSUURLProtocolHandledKey"
 
